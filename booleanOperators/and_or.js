2136e56c42a8cab40fe836a0515ce47c5c11d9d2
mcvaysea <mcvay.sean@gmail.com>
2023-02-17 15:26:01 -0800

    complete bool and operators

---
+++ booleanOperators/and_or.js
@@ -0,0 +1,18 @@
+console.log("<------Boolean Operators------>");
+
+console.log("<----Logical AND---->");
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+console.log("<----Logical OR---->");
+console.log(true || true);
+console.log(true || false);
+console.log(true || false);
+console.log(false || false);
+
+console.log("<----Logical NOT---->");
+
+console.log(!true);
+console.log(!false);
---
+++ booleanOperators/booleanOperators.js
@@ -0,0 +1,29 @@
+console.log("<------Boolean Operators------>");
+
+console.log("<----Strict Equality---->");
+
+console.log(5 === 5);
+console.log(5 === 4);
+console.log(4 === 5);
+
+
+console.log("<----Strict Not Equality---->");
+
+console.log(5 !== 5);
+console.log(4 !== 5);
+console.log(5 !== 4);
+
+console.log("<----GreaterThan / LessThan---->");
+
+console.log(9 > 7);
+console.log(6 < 5);
+console.log(3 < 3);
+console.log(3 <= 3);
+console.log(4 >= 2);
+
+
+console.log("<----Logical AND/OR---->");
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
