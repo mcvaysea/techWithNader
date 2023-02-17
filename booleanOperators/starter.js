75c6769b463f5bc93845aa9b87e5ca116e180f72
mcvaysea <mcvay.sean@gmail.com>
2023-02-17 12:25:43 -0800

    update starter file

--- booleanOperators/starter.js
+++ booleanOperators/starter.js
@@ -1,8 +1,27 @@
-
 ///////////////////////////////////////////////////////
 //////////////BOOLEAN OPERATORS IN JS/////////////////
 /////////////////////////////////////////////////////
 
+/*
+== Equal To
 
+=== Strict Equal To
 
+!= Not Equal To
 
+!== Strict Not Equal To
+
+> Greater Than
+
+< Less Than
+
+>= Greater Than or Equal To
+
+<= Less Than or Equal to
+
+&& Logical AND
+
+|| Logical OR
+
+?? Nullish Coalescing
+*/
